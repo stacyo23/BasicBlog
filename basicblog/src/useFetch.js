3fd1735922c2eef4489d58f1dc874af7c503b25f
Stacy Betts <stacyo23@yahoo.com>
2021-02-16 09:10:47 -0600

    first commit, created navbar, blogs, custom hook, etc

---
+++ basicblog/src/useFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'; 
+
+const useFetch = (url) => {
+
+    const[data, setData] = useState(null); 
+    const[isPending, setIsPending] =useState(true);
+    const[error, setError] =useState(null);
+
+
+    useEffect(() => {
+        const abortCont = new AbortController(); 
+
+        fetch(url, {signal: abortCont.signla})
+        .then(res => {
+            if(!res.ok) {
+                throw Error("could not fetch data for that resource")
+            }
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data)
+            setData(data)
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            if (err.name === 'AbortError') {
+                console.log('fetch aborted');
+            }
+            setIsPending(false);
+            setError(err.message);
+        })
+        return () => {
+            abortCont.abort();
+        }
+
+    }, [url]);
+
+    return { data, isPending, error }
+}
+
+export default useFetch; 
